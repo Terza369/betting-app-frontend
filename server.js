59f93c9a978235c9f7194a4a21c6a8e250bdd90e
Terza <simun.terzanovic@gmail.com>
2021-03-08 02:25:41 +0100

    Routing fix

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const path = require('path')
 
 const app = express()
 
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.use('/', serveStatic(path.join(__dirname, '/dist')));
+
+app.get(/.*/, function (req, res) {
+	res.sendFile(path.join(__dirname, '/dist/index.html'))
+});
 
 app.listen(process.env.PORT || 8080, () => {
     console.log(`Listening on port: ${process.env.PORT || 8080}`);
